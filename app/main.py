c643e2500365085bd91ed33c056a39f47696f788
Shakila shaik <skshakilask67@gmail.com>
2025-08-14 12:07:21 +0530

    changed the urls to dynamic-3

--- app/main.py
+++ app/main.py
@@ -2,16 +2,17 @@ from fastapi import FastAPI
 from app.routes import router
 from fastapi.middleware.cors import CORSMiddleware
 
-
+import os
 app = FastAPI()
+frotnend_origin=os.getenv("ALLOWED_ORIGINS")
 
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173"
-    ],  # Replace "*" with specific origins in production
+    allow_origins=
+        frotnend_origin
+    ,  # Replace "*" with specific origins in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
